508bb0f5bd6a37d8ef2b4d8d0245c69fd08ce520
sprit-maberasu-1140-source <>
2026-05-11 14:14:15 +0000

    Challenge: Fixing Syntax Errors

---
+++ syntax_and_indentation_pitfalls/challenge_fixing_syntax_errors/main.py
@@ -0,0 +1,16 @@
+def print_greeting():
+    print("Hello, Python learner!")
+
+def calculate_sum(a, b):
+    return a + b
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print_greeting()
+print("Sum:", calculate_sum(3, 5))
+print("Is 4 even?", is_even(4))
+print("Is 7 even?", is_even(7))
