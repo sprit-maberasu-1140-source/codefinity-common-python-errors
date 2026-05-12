0fd6b49375bc08e13fb39ca3fa0229017c41fce7
sprit-maberasu-1140-source <>
2026-05-12 05:12:04 +0000

    Challenge: Variable Name Fixer

---
+++ variable_and_assignment_mistakes/challenge_variable_name_fixer/main.py
@@ -0,0 +1,8 @@
+def fix_variable_names():
+    first_value = 10
+    my_value = 5
+    for_value = 7
+    total = first_value + my_value + for_value
+    print("Total:", total)
+
+fix_variable_names()
